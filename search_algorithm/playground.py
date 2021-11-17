df74a5c7865c73cae6eb6f695eb2adf68ced3cf4
Ryuku-sketch <cyamy122002@gmail.com>
2021-11-17 22:40:51 +0900

    Refactor

--- search_algorithm/playground.py
+++ search_algorithm/playground.py
@@ -1,16 +1,15 @@
 #%%
 #import and define necessary function and variable
 import universal_function as uf
-answer = uf.getPassword()
-#%%
 import search_algorithm.bruteforce as bf
 
-
+answer = uf.getPassword()
 
 
 #%%
 #see how fast simple brute force reveals the password
 
-bf.SimpleBurteForece('answer')
-bf.adjBruteForece(answer)
+bf.SimpleBurteForece(answer)
 # %%
+bf.adjBruteForece(answer)
+
